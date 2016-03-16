ceefb845a047c3a33934471332832bee8807b4d6
Nick Dunne <imdunne8@gmail.com>
2016-03-16 16:53:59 -0500

    - Added some basic OpenCV processing
    - HSV threshold and contours, sets SD boolean

--- src/main/java/edu/wpi/first/smartdashboard/gui/elements/MJPGStreamerViewerExtension.java
+++ src/main/java/edu/wpi/first/smartdashboard/gui/elements/MJPGStreamerViewerExtension.java
@@ -6,6 +6,7 @@ import edu.wpi.first.smartdashboard.properties.IntegerProperty;
 import edu.wpi.first.smartdashboard.properties.Property;
 import edu.wpi.first.smartdashboard.properties.StringProperty;
 import edu.wpi.first.wpilibj.networktables.*;
+import edu.wpi.first.wpilibj.tables.ITable;
 
 import java.awt.BasicStroke;
 import java.awt.Color;
@@ -15,19 +16,31 @@ import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.geom.AffineTransform;
 import java.awt.image.BufferedImage;
+import java.awt.image.DataBufferByte;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 import java.net.URLConnection;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Vector;
 
 import javax.imageio.ImageIO;
 
 import org.jcodec.api.awt.SequenceEncoder8Bit;
+import org.jcodec.common.io.IOUtils;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfPoint;
+import org.opencv.imgproc.Imgproc;
+import org.opencv.imgproc.Moments;
 
 /**
  *
@@ -36,11 +49,11 @@ import org.jcodec.api.awt.SequenceEncoder8Bit;
 public class MJPGStreamerViewerExtension extends StaticWidget {
 
     /**
-	 * 
-	 */
-	private static final long serialVersionUID = 3280022425547727170L;
+     * 
+     */
+    private static final long serialVersionUID = 3280022425547727170L;
 
-	public static final String NAME = "MJPG Streamer Camera Switcher";
+    public static final String NAME = "MJPG Streamer Camera Switcher";
     
     private NetworkTable networkTable; 
 
@@ -61,6 +74,7 @@ public class MJPGStreamerViewerExtension extends StaticWidget {
     private int crosshairY = 0;
     private int crosshairSize = 0;
     private boolean swapCameras = false;
+    private boolean processOpenCV = false;
     private Font camFont = new Font("Arial", Font.BOLD, 20);
     private SequenceEncoder8Bit encoder = null;
     public class BGThread extends Thread {
@@ -93,13 +107,13 @@ public class MJPGStreamerViewerExtension extends StaticWidget {
                     connection2.setReadTimeout(250);
                     stream2 = connection2.getInputStream();
                     try {
-                    	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
-                    	String currentTime = LocalDateTime.now().format(formatter);
-                    	System.out.println("Creating file " + currentTime + "_camFeed4.mp4");
-        				encoder = new SequenceEncoder8Bit(new File(currentTime + "_camFeed.mp4"));
-        			} catch (IOException e1) {
-        				System.out.println("Error: Encoder could not be created");
-        			}
+                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd_HH-mm-ss");
+                        String currentTime = LocalDateTime.now().format(formatter);
+                        System.out.println("Creating file " + currentTime + "_camFeed4.mp4");
+                        encoder = new SequenceEncoder8Bit(new File(currentTime + "_camFeed.mp4"));
+                    } catch (IOException e1) {
+                        System.out.println("Error: Encoder could not be created");
+                    }
                     networkTable = NetworkTable.getTable("SmartDashboard");
 
                     while(!destroyed && !ipChanged){
@@ -112,8 +126,9 @@ public class MJPGStreamerViewerExtension extends StaticWidget {
                         stream2.skip(stream2.available());
                         
                         useCamera1 = networkTable.getBoolean("useCamera1", true);
+                        processOpenCV = networkTable.getBoolean("processOpenCV", false);
                         if(swapCameras)
-                        	useCamera1 = !useCamera1;
+                            useCamera1 = !useCamera1;
 
                         imageBuffer.reset();
                         for(int i = 0; i<START_BYTES.length;){
@@ -148,7 +163,9 @@ public class MJPGStreamerViewerExtension extends StaticWidget {
                         ByteArrayInputStream tmpStream = new ByteArrayInputStream(imageBuffer.toByteArray());
                         imageToDraw = ImageIO.read(tmpStream);
                         if(encoder != null)
-                        	encoder.encodeImage(imageToDraw);
+                            encoder.encodeImage(imageToDraw);
+                        if(processOpenCV)
+                            ProcessImageOpenCV(imageToDraw);
                         repaint();
                     }
 
@@ -156,15 +173,15 @@ public class MJPGStreamerViewerExtension extends StaticWidget {
                     imageToDraw = null;
                     repaint();
                     System.out.println("Error: Connection to camera failed");
-					try {
-						if(encoder != null)
-						{
-							encoder.finish();
-							encoder = null;
-						}
-					} catch (IOException e1) {
-						System.out.println("Error: Encoder unable to finish");
-					}
+                    try {
+                        if(encoder != null)
+                        {
+                            encoder.finish();
+                            encoder = null;
+                        }
+                    } catch (IOException e1) {
+                        System.out.println("Error: Encoder unable to finish");
+                    }
                 }
 
                 if(!ipChanged){
@@ -180,14 +197,14 @@ public class MJPGStreamerViewerExtension extends StaticWidget {
         public void destroy() {
             destroyed = true;
             try {
-            	if(encoder != null)
-            	{
-            		encoder.finish();
-            		encoder = null;
-            	}
-			} catch (IOException e) {
-				System.out.println("Error: Encoder unable to finish");
-			}
+                if(encoder != null)
+                {
+                    encoder.finish();
+                    encoder = null;
+                }
+            } catch (IOException e) {
+                System.out.println("Error: Encoder unable to finish");
+            }
         }
     }
     private BufferedImage imageToDraw;
@@ -204,6 +221,7 @@ public class MJPGStreamerViewerExtension extends StaticWidget {
 
     @Override
     public void init() {
+        loadLibrary();
         setPreferredSize(new Dimension(160, 120));
         ipString = ipProperty.getSaveValue();
         rotateAngleRad = Math.toRadians(rotateProperty.getValue());
@@ -229,42 +247,42 @@ public class MJPGStreamerViewerExtension extends StaticWidget {
             rotateAngleRad = Math.toRadians(rotateProperty.getValue());
         }
         if (property == rotate2Property) {
-        	rotate2AngleRad = Math.toRadians(rotate2Property.getValue());
+            rotate2AngleRad = Math.toRadians(rotate2Property.getValue());
         }
         if (property == portProperty) {
             port = portProperty.getValue();
             ipChanged = true;
         }
         if (property == port2Property) {
-        	port2 = port2Property.getValue();
-        	ipChanged = true;
+            port2 = port2Property.getValue();
+            ipChanged = true;
         }
         if (property == crosshairXProperty) {
-        	crosshairX = crosshairXProperty.getValue();
+            crosshairX = crosshairXProperty.getValue();
         }
         if (property == crosshairYProperty) {
-        	crosshairY = crosshairYProperty.getValue();
+            crosshairY = crosshairYProperty.getValue();
         }
         if (property == crosshairSizeProperty) {
-        	crosshairSize = crosshairSizeProperty.getValue();
+            crosshairSize = crosshairSizeProperty.getValue();
         }
         if(property == swapCamerasProperty) {
-        	swapCameras = swapCamerasProperty.getValue();
+            swapCameras = swapCamerasProperty.getValue();
         }
     }
 
     @Override
     public void disconnect() {
-			try {
-		    	if(encoder != null)
-		    	{
-		    		encoder.finish();
-		    		encoder = null;
-		    	}
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				System.out.println("Error: Encoder unable to finish");
-			}
+            try {
+                if(encoder != null)
+                {
+                    encoder.finish();
+                    encoder = null;
+                }
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                System.out.println("Error: Encoder unable to finish");
+            }
         bgThread.destroy();
         super.disconnect();
     }
@@ -274,7 +292,7 @@ public class MJPGStreamerViewerExtension extends StaticWidget {
         BufferedImage drawnImage = imageToDraw;
 
         if (drawnImage != null) {
-        	double rotate = useCamera1 ? rotateAngleRad : rotate2AngleRad;
+            double rotate = useCamera1 ? rotateAngleRad : rotate2AngleRad;
             // cast the Graphics context into a Graphics2D
             Graphics2D g2d = (Graphics2D)g;
 
@@ -321,19 +339,19 @@ public class MJPGStreamerViewerExtension extends StaticWidget {
             g2d.setFont(camFont);
             if(useCamera1)
             {
-            	g2d.setColor(Color.GREEN);
-            	g2d.drawString("CAM 1", 80, 20);
+                g2d.setColor(Color.GREEN);
+                g2d.drawString("CAM 1", 80, 20);
             }
             else
             {
-            	g2d.setColor(Color.MAGENTA);
-            	g2d.drawString("CAM 2", 80, 20);
+                g2d.setColor(Color.MAGENTA);
+                g2d.drawString("CAM 2", 80, 20);
             }
             
             if(crosshairSize > 0)
             {
-            	g2d.setColor(Color.YELLOW);
-            	g2d.drawOval(crosshairX, crosshairY, crosshairSize, crosshairSize);
+                g2d.setColor(Color.YELLOW);
+                g2d.drawOval(crosshairX, crosshairY, crosshairSize, crosshairSize);
             }
         } else {
             g.setColor(Color.PINK);
@@ -341,5 +359,94 @@ public class MJPGStreamerViewerExtension extends StaticWidget {
             g.setColor(Color.BLACK);
             g.drawString("NO CONNECTION", 10, 10);
         }
+    }
+    
+    private static void loadLibrary()
+    {
+        try
+        {
+            InputStream in = null;
+            File fileOut = null;
+            in = new FileInputStream("C:\\OpenCV\\opencv\\build\\java\\x64\\opencv_java310.dll");
+            fileOut = File.createTempFile("lib",".dll");
+            OutputStream out = new FileOutputStream(fileOut);
+            IOUtils.copy(in, out);
+            in.close();
+            out.close();
+            System.load(fileOut.toString());
+        }
+        catch(Exception ex)
+        {
+            throw new RuntimeException("Failed to load opencv native library", ex);
+        }
+    }
+    
+    private void ProcessImageOpenCV(BufferedImage im)
+    {
+        // Threshold values for HSV image filter
+        int HMin = 0;
+        int HMax = 255;
+        int SMin = 0;
+        int SMax = 255;
+        int VMin = 0;
+        int VMax = 255;
+        
+        Mat mat = new Mat(im.getHeight(), im.getWidth(), CvType.CV_8UC3);
+        byte[] data = ((DataBufferByte) im.getRaster().getDataBuffer()).getData();
+        mat.put(0, 0, data);
+        //Convert image to HSV, then split to separate channels
+        Imgproc.cvtColor(mat, mat, Imgproc.COLOR_RGB2HSV);
+        Vector<Mat> split = new Vector<Mat>(3);
+        Core.split(mat, split);
+        Mat mH = split.get(0);
+        Mat mS = split.get(1);
+        Mat mV = split.get(2);
+        //Perform thresholds
+        Imgproc.threshold(mH, mH, HMin, HMax, Imgproc.THRESH_BINARY);
+        //Imgproc.threshold(mH, mH, HMax, 255, Imgproc.THRESH_BINARY_INV);
+        Imgproc.threshold(mS, mS, SMin, SMax, Imgproc.THRESH_BINARY);
+        //Imgproc.threshold(mS, mS, SMax, 255, Imgproc.THRESH_BINARY_INV);
+        Imgproc.threshold(mV, mV, VMin, VMax, Imgproc.THRESH_BINARY);
+        //Imgproc.threshold(mV, mV, VMax, 255, Imgproc.THRESH_BINARY_INV);
+        //And the channels back together
+        Core.bitwise_and(mH, mS, mS);
+        Core.bitwise_and(mS, mV, mV);
+        Mat hierarchy = new Mat();
+        //Find contours, then decide to shoot based on x/y
+        Vector<MatOfPoint> contours = new Vector<MatOfPoint>();
+        Imgproc.findContours(mV, contours, hierarchy, Imgproc.RETR_LIST, Imgproc.CHAIN_APPROX_SIMPLE);
+//      Vector<Double> width = new Vector<Double>();
+//      Vector<Double> height= new Vector<Double>();
+//      Vector<Double> centerX = new Vector<Double>();
+//      Vector<Double> centerY = new Vector<Double>();
+//      Vector<Double> area = new Vector<Double>();
+        boolean shotAligned = false;
+        for(MatOfPoint contour:contours)
+        {
+//          width.add((double)contour.width());
+//          height.add((double)contour.height());
+            Moments moments = Imgproc.moments(contour);
+            double pointCenterX = moments.get_m10() / moments.get_m00();
+            double pointCenterY = moments.get_m01() / moments.get_m00();
+//          centerX.add(pointCenterX);
+//          centerY.add(pointCenterY);
+//          area.add(Imgproc.contourArea(contour));
+            double xError = Math.abs(pointCenterX - crosshairX) / crosshairX;
+            double yError = Math.abs(pointCenterY - crosshairY) / crosshairY;
+            if(xError < 0.1 && yError < 0.1)
+                shotAligned = true;
+        }
+//      ITable targetTable = NetworkTable.getTable("grip").getSubTable("targets");
+//      if(targetTable != null)
+//      {
+//          targetTable.putNumberArray("width", (Double[])width.toArray());
+//          targetTable.putNumberArray("height", (Double[])height.toArray());
+//          targetTable.putNumberArray("center_x", (Double[])centerX.toArray());
+//          targetTable.putNumberArray("center_y", (Double[])centerY.toArray());
+//          targetTable.putNumberArray("area", (Double[])area.toArray());
+//      }
+        
+        //if a found contour was close to the expected target center, write true to smartdashboard "shoot"
+        networkTable.putBoolean("ShotAligned", shotAligned);
     }
 }
